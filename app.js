5970f9e3a91752ba5d6633b9c3338b3478e84a38
JoseOneTime <claveren@gmail.com>
2014-01-19 19:32:18 -0500

    Add route to home view

--- app.js
+++ app.js
@@ -4,10 +4,9 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var store = require('./routes/store')
 
 var app = express();
 
@@ -31,8 +30,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', store.home)
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
